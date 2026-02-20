9e7eb13169e9bcd2aef2af1a78d336a964a4ae1c
shekhar <shekhart442@gmail.com>
2026-02-20 19:25:08 +0530

    OOPs added

--- practice_08.py
+++ practice_08.py
@@ -1,8 +1,19 @@
+# OOPs (Object Oriented Programming ) Classes & Objects
+
 class Student:
-    name = input("Enter student name: ")
 
-s1 = Student()
-print(Student.name)
+    # default constructors
+    def __init__(self):
+        pass
 
-s2 = Student()
-print(Student.name)
+    # parameterized constructors
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("adding new data of students in the database...")
+
+s1 = Student("John Cena", 99)
+print(s1.name, s1.marks)
+
+s2 = Student("Roman", 76)
+print(s2.name, s2.marks)
