821d1114f05e179f54b2c91158cd0043e5c3c533
shekhar <shekhart442@gmail.com>
2026-02-23 14:32:54 +0530

    done revision in OOPs

--- practice_08.py
+++ practice_08.py
@@ -1,5 +1,6 @@
 # OOPs (Object Oriented Programming ) Classes & Objects
 
+'''
 class Student:
 
     # default constructors
@@ -17,3 +18,29 @@ print(s1.name, s1.marks)
 
 s2 = Student("Roman", 76)
 print(s2.name, s2.marks)
+'''
+
+# Create student class that takes name & marks of 3 subjects as arguments in constructor. Then create a method to print the average
+
+class Student:
+    def __init__(self, name, marks, subjects):
+        self.name = name
+        self.marks = marks
+        self.subjects = subjects
+        print("Adding new data of students in the DB...")
+
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("HEY", self.name, "your avg score is:", sum/3)
+
+    @staticmethod
+    def hello():
+        print("Hello!")
+
+s1 = Student("John Cena", [99,77,32,56], "Computer Science")
+# print(s1.name,"|",s1.marks,"|",s1.subjects)
+s1.get_avg()
+
+s1.hello()
